80c4d76426e92df307c06ed5f7a7001b0bf7c919
RK Kuppala <ranjeeth.kuppala@gmail.com>
2021-02-08 11:22:39 +0530

    initial commit

---
+++ cloud-spanner/blueprint/variables.tf
@@ -0,0 +1,7 @@
+## --- REQUIRED PARAMETERS ------------------------------------------------------------------------------------------------
+
+variable "gcp_project_id" {
+  type        = string
+  default     = "[gcp-project-id]"
+  description = "Id of the GCP project"
+}
