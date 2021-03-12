c3a099e8e8c4844b314ad14b769e4cb1020922ed
vdr_rohan.singh <vdr_rohan.singh@airasia.com>
2021-03-12 20:14:45 +0530

    update gce metadata with script

--- examples/gce/providers.tf
+++ examples/gce/providers.tf
@@ -4,6 +4,6 @@ terraform {
 
 provider "google" {
   version = "3.51.0" # see https://github.com/terraform-providers/terraform-provider-google/releases
-  project = "gcp-project-id"
+  project = "<gcp-project-id>"
   region  = var.region
 }
