145b36a2e36aadaa42ef1cddd1c97ab1277361a9
vigneshwaran <78083959+vignesh-sdev@users.noreply.github.com>
2021-04-30 18:16:59 +0530

    Update modules/cloud-spanner/variables.tf
    Co-authored-by: Knut Olav Løite <koloite@gmail.com>

--- modules/cloud-spanner/variables.tf
+++ modules/cloud-spanner/variables.tf
@@ -54,7 +54,7 @@ variable "ddl_queries" {
     "CREATE TABLE users ( userId STRING(36) NOT NULL, businessEmail STRING(50), fullName STRING(36), password STRING(100), photoUrl STRING(250), provider STRING(20),) PRIMARY KEY(userId)",
     "CREATE UNIQUE NULL_FILTERED INDEX usersByBusinessEmail ON users (businessEmail)",
     "CREATE TABLE companies (companyId STRING(36) NOT NULL, companyName STRING(30), companyShortCode STRING(15), created_at TIMESTAMP NOT NULL OPTIONS (allow_commit_timestamp = true), ) PRIMARY KEY(companyId)",
-    "CREATE UNIQUE NULL_FILTERED INDEX companiesByCompanyName ON companies (companyName",
+    "CREATE UNIQUE NULL_FILTERED INDEX companiesByCompanyName ON companies (companyName)",
     "CREATE UNIQUE NULL_FILTERED INDEX companiesByShortCode ON companies (companyShortCode)",
     "CREATE TABLE companyStocks (companyStockId STRING(36) NOT NULL, companyId STRING(36) NOT NULL, open NUMERIC, volume NUMERIC, currentValue NUMERIC, date FLOAT64, close NUMERIC, dayHigh NUMERIC, dayLow NUMERIC, timestamp TIMESTAMP NOT NULL OPTIONS (allow_commit_timestamp=true), CONSTRAINT FK_CompanyStocks FOREIGN KEY (companyId) REFERENCES companies (companyId)) PRIMARY KEY(companyStockId)",
     "CREATE TABLE simulations (sId STRING(36) NOT NULL, companyId STRING(36) NOT NULL, status STRING(36), createdAt TIMESTAMP NOT NULL OPTIONS (allow_commit_timestamp=true), CONSTRAINT FK_CompanySimulation FOREIGN KEY (companyId) REFERENCES companies (companyId)) PRIMARY KEY(sId)",
